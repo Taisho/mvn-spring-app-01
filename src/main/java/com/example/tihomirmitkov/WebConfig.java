6357f065bf7e1b1f4d7f4e7c271fa66240d8ebcd
Tihomir Mitkov <mitkovtihomir@gmail.com>
2020-03-30 00:21:02 +0300

    Adding comments

--- src/main/java/com/example/tihomirmitkov/WebConfig.java
+++ src/main/java/com/example/tihomirmitkov/WebConfig.java
@@ -8,30 +8,30 @@ import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-
+//NOTE subclassing WebMvcConfigurationSupport is like annotating with @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = {"com.example.tihomirmitkov"})
 public class WebConfig extends WebMvcConfigurationSupport {
 
-    /*@Autowired
-    private ApplicationContext applicationContext;
-*/
+    /* Only by having this method static resources get served. */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/../webapp");
+                .addResourceLocations("classpath:/");
     }
 
+    /* as if this used to work during my experiments, but now doesn't seem to */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index.jsp");
+        registry.addViewController("/").setViewName("index1.jsp");
         registry.addViewController("/index").setViewName("index.jsp");
     }
 
 
+    /* The code here is practically a no-op. */
     @Override
     public void configureViewResolvers(final ViewResolverRegistry registry) {
-        //registry.jsp("classpath:/", ".jsp");
+        //registry.jsp("classpath:/", ".jsp");  //this doesn't work either
 
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/");
